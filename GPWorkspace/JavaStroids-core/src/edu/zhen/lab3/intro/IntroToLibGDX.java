128802b571ab62ce9265284d4b7b89a0a648e81d
Benny Zhen <bluehelper358@gmail.com>
2014-10-04 14:32:26 -0400

    lab3 assignment done

--- GPWorkspace/JavaStroids-core/src/edu/zhen/lab3/intro/IntroToLibGDX.java
+++ GPWorkspace/JavaStroids-core/src/edu/zhen/lab3/intro/IntroToLibGDX.java
@@ -13,6 +13,10 @@ public class IntroToLibGDX extends ApplicationAdapter{
 	private Sprite bug;
 	private Sprite chest;
 	private float rotDeg;
+	private float x=30, y =0;
+	private final float speed = 10.0f;
+	private float deltaTime;
+	private boolean reachTop = false;
 	
 	@Override
 	public void create() {
@@ -21,12 +25,13 @@ public class IntroToLibGDX extends ApplicationAdapter{
 		bug = new Sprite(new Texture("EnemyBug.png"));
 		bug.setSize(50, 85);
 		bug.setOrigin(bug.getWidth() / 2, bug.getHeight() / 2);
-		bug.setPosition(200, 200);
+		bug.setPosition(30, 0);
 		chest = new Sprite(new Texture("ChestClosed.png"));
 		chest.setSize(50, 85);
 		chest.setOrigin(chest.getWidth()/2, chest.getHeight()/2);
 		chest.setPosition(270, 240);
-		rotDeg = 3;
+		bug.rotate(20);
+		rotDeg = 180;
 	}
 
 	@Override
@@ -35,7 +40,46 @@ public class IntroToLibGDX extends ApplicationAdapter{
 		Gdx.gl.glClearColor(0.7f, 0.7f, 0.2f, 1);
 	    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		spriteBatch.begin();
-		bug.rotate(rotDeg);
+		
+		deltaTime = Gdx.graphics.getDeltaTime();
+		
+		if(!reachTop){
+			x += (speed * deltaTime) + 0.12f;
+			y += (speed * deltaTime);
+		}
+		
+		if(reachTop){
+			x -= (speed * deltaTime) + 0.12f;
+			y -= (speed * deltaTime);
+		}
+		
+		if(x >= 750 && y >= 420 || x <= 0 && y <= 0){
+			if(reachTop){
+				reachTop = false;
+				bug.rotate(rotDeg);
+			}
+			else{
+				reachTop = true;
+				bug.rotate(rotDeg);
+			}
+		}
+		
+		/*
+		if(x >= 750 && y >= 420){
+			bug.rotate(rotDeg);
+		}
+		
+		if(x <= 0 && y <= 0){
+			bug.rotate(rotDeg);
+		}
+		
+		
+		System.out.println("x = " + x);
+		System.out.println("y = " + y);
+		*/
+		
+		bug.setPosition(x, y);
+		
 		bug.draw(spriteBatch);
 		chest.draw(spriteBatch);
 		spriteBatch.end();
